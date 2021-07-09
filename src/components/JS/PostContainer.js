15cb8787bd36aca21f03e35aa057c831d63021c7
TylerL <tluckewicz@gmail.com>
2021-07-09 09:12:06 -0400

    add Post component

--- src/components/JS/PostContainer.js
+++ src/components/JS/PostContainer.js
@@ -1,9 +1,13 @@
 import '../CSS/PostContainer.css';
+import Post from './Post';
 
 function PostContainer() {
     return (
         <div className='post-container'>
             This is the post container
+            <Post />
+            <Post />
+            <Post />
         </div>
     );
 }
