29cd5ca1eeb1f9150b24060a11d1b0e352744337
TylerL <tluckewicz@gmail.com>
2021-07-09 09:06:36 -0400

    set up PostContainer component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import PostContainer from './components/JS/PostContainer';
 
 function App() {
   return (
     <div className="App">
       <h1>Dev Study</h1>
+      <PostContainer />
     </div>
   );
 }
---
+++ src/components/JS/PostContainer.js
@@ -0,0 +1,9 @@
+function PostContainer() {
+    return (
+        <div>
+            This is the post container
+        </div>
+    );
+}
+
+export default PostContainer;
